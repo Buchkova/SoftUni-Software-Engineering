95cc42b32d18c4103926df24d6f0b7b011b41c5f
Buchkova <inesbuchkova@gmail.com>
2022-05-28 20:01:28 +0100

    Adding Arrays Exercises

---
+++ Programming Fundamentals with C# - September 2021/03.Arrays.Exercise/02.CommonElements/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.CommonElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrOne = Console.ReadLine().Split(" ");
+            string[] arrTwo = Console.ReadLine().Split(" ");
+
+            foreach (var elementsTwo in arrTwo)
+            {
+                for (int i = 0; i < arrOne.Length; i++)
+                {
+                    string elementsOne = arrOne[i];
+                    if (elementsTwo == elementsOne)
+                    {
+                        Console.Write(elementsOne + " ");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}

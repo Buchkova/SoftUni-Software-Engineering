ad59f507309023f7952a10a9c9445ad74cd9710f
Buchkova <inesbuchkova@gmail.com>
2022-06-01 23:11:22 +0100

    Lesson for Associative Arrays

---
+++ Programming Fundamentals with C# - September 2021/08.AssociativeArrays/01.CountRealNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            SortedDictionary<double, int> conuts = new SortedDictionary<double, int>();
+
+            foreach (var number in numbers)
+            {
+                if (conuts.ContainsKey(number))
+                {
+                    conuts[number]++;
+                }
+                else
+                {
+                    conuts.Add(number, 1);
+                }
+            }
+
+            foreach (var count in conuts)
+            {
+                Console.WriteLine($"{count.Key} -> {count.Value}");
+            }
+            
+
+        }
+    }
+}

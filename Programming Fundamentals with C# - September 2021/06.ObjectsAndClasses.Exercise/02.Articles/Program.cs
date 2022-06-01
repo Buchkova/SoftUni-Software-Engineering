65cc96c9cc71c03094544bf8f00f6e0f600b34aa
Buchkova <inesbuchkova@gmail.com>
2022-06-01 23:08:28 +0100

    Adding exercises from Objects and Classes

---
+++ Programming Fundamentals with C# - September 2021/06.ObjectsAndClasses.Exercise/02.Articles/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace T02._Articles.ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] currArticle = Console.ReadLine().Split(", ");
+            Article article = new Article(currArticle[0], currArticle[1], currArticle[2]);
+            int NumOfCommands = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < NumOfCommands; i++)
+            {
+                string[] cmd = Console.ReadLine().Split(": ");
+                string mainCmd = cmd[0];
+                string argument = cmd[1];
+
+                switch (mainCmd)
+                {
+                    case "Edit":
+                        article.Edit(argument);
+                        break;
+
+                    case "ChangeAuthor":
+                        article.ChangeAuthor(argument);
+                        break;
+
+                    case "Rename":
+                        article.Rename(argument);
+                        break;
+                }
+            }
+            Console.WriteLine(article);
+        }
+    }
+    class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+
+        public void Edit(string content) => Content = content;
+        public void ChangeAuthor(string author) => Author = author;
+        public void Rename(string title) => Title = title;
+    }
+}
---
+++ Programming Fundamentals with C# - September 2021/06.ObjectsAndClasses.Exercise/07.OrderByAge/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderByAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var people = new List<Person>();
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] data = input.Split();
+                string name = data[0];
+                string id = data[1];
+                int age = int.Parse(data[2]);
+
+                var person = new Person(name, id, age);
+
+                if (people.Any(x => x.ID == id))
+                {
+                    foreach (Person personn in people)
+                    {
+                        if (personn.ID == id)
+                        {
+                            personn.Name = name;
+                            personn.Age = age;
+                        }
+                    }
+                }
+                else
+                {
+                    people.Add(person);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (Person person in people.OrderBy(x => x.Age))
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
+
+class Person
+{
+    public string Name { get; set; }
+    public string ID { get; set; }
+    public int Age { get; set; }
+
+    public Person(string name, string id, int age)
+    {
+        this.Name = name;
+        this.ID = id;
+        this.Age = age;
+    }
+
+    public void EditNameAndAge(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} with ID: {ID} is {Age} years old.";
+    }
+}
+

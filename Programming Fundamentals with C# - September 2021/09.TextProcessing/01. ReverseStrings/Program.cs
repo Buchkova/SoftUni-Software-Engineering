1f3edd06c5e880cfc285af97581667f60272ff58
Buchkova <inesbuchkova@gmail.com>
2022-06-07 16:27:37 +0100

    Text Processing lesson

---
+++ Programming Fundamentals with C# - September 2021/09.TextProcessing/01. ReverseStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01._ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            string reversed = "";
+            while (line != "end")
+            {
+                for (int i = line.Length - 1; i >= 0; i--)
+                {
+                    reversed += line[i];
+                    
+                }
+                Console.WriteLine($"{line} = {reversed}");
+                reversed = "";
+                line = Console.ReadLine();
+                
+            }
+            
+        }
+    }
+}

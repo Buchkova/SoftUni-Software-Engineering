43b92ed03c29f043e4c9c3570d4638120c6bd724
Ines Buckova <inesbuchkova@gmail.com>
2022-06-11 20:12:37 +0100

    C# Advanced with the first Lab for Stacks and Queues

---
+++ C# Advanced - May 2022/StacksAndQueues/07.HotPotato/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.HotPotato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int number = int.Parse(Console.ReadLine());
+            Queue<string> kids = new Queue<string>(input);
+
+            while (kids.Count > 1)
+            {
+                for (int i = 1; i < number; i++)
+                {
+                    kids.Enqueue(kids.Dequeue());
+                }
+                Console.WriteLine($"Removed {kids.Dequeue()}");
+            }
+            Console.WriteLine($"Last is {kids.Dequeue()}");
+            
+
+        }
+    }
+}
+//Create a program that simulates the game of Hot Potato. Print every kid that is removed from the circle. In the end, print the kid that is left last.

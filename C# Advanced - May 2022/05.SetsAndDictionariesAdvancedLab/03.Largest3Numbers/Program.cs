f75a075aca4e51cbd0875278830e3835ab43227e
Ines Buckova <inesbuchkova@gmail.com>
2022-07-23 17:50:27 +0100

    Sets And Dictionaries Advanced Lab

---
+++ C# Advanced - May 2022/05.SetsAndDictionariesAdvancedLab/03.Largest3Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.Largest3Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Largest 3 Numbers
+
+            //Read a list of integers and print the largest 3 of them. If there are less than 3, print all of them.
+
+            //Read an array of integers.
+            //Order the array using a LINQ query.
+            //Print top 3 numbers with a for loop.
+
+
+            List<int> numbers = new List<int>(Console.ReadLine().Split().Select(int.Parse).ToList());
+            numbers.OrderByDescending(x => x).ToList();
+            var output = numbers.OrderByDescending(x => x).ToList().Take(3);
+            Console.WriteLine(string.Join(" ", output));
+        }
+    }
+}
